c585767b6bb534af0302cc212c8b2aa1e2095098
Damien Metzler <dmetzler@nuxeo.com>
2016-07-27 17:24:11 +0200

    NXIO-548 don't check java version since it's buggy

--- start.sh
+++ start.sh
@@ -59,6 +59,9 @@ if [ ! -z "$CONNECT_URL" ]; then
   echo "org.nuxeo.connect.url=$CONNECT_URL" >> $NUXEO_CONF
 fi
 
+
+echo "JAVA_OPTS=$JAVA_OPTS -Djvmcheck=nofail" >> $NUXEO_CONF
+
 # Start nuxeo
 su $NUXEO_USER -m -c "$NUXEOCTL --quiet start"
 su $NUXEO_USER -m -c "tail -f /var/log/nuxeo/server.log"
